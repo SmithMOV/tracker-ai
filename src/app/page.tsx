83c663a187029d78499d34542c0c63ac9d49988e
Vardan <vardan.vanyan@gmail.com>
2023-11-18 18:39:13 -0800

    added DB schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
-import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
     <div className="relative bg-gradient-to-r from-blue-500 to-purple-500 h-screen flex items-center justify-center overflow-hidden">
-      {/* <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: 'url("/path/to/your/animated-bg.gif")' }}
-      ></motion.div> */}
-
       <div className="absolute inset-0 bg-black opacity-50 z-1"></div>
 
-      <div className="relative z-2 mx-auto max-w-2xl text-center text-white">
+      <div className="mx-auto max-w-4xl sm:px-6 lg:px-8 relative z-2 text-center text-white">
         <h2 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
           <span className="text-yellow-300">Revolutionize your job search</span>{' '}
-          with our <span className="text-green-300">Job Tracker AI</span>!
+          with our{' '}
+          <span className="text-green-300 transform transition-transform hover:scale-105 duration-300">
+            Job Tracker AI
+          </span>
+          !
         </h2>
         <p className="mx-auto mt-6 max-w-xl text-lg leading-8">
           Elevate your job hunting experience with cutting-edge technology and
